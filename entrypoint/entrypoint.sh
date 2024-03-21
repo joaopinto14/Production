ef22c2cf159885d6f7cf0131239721c7cb8955d4
João Pinto <psp.joao@hotmail.com>
2024-03-21 19:27:15 +0000

    Em modificação

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -6,6 +6,36 @@ PROJECT_PATH=${PROJECT_PATH:-/var/www/html}
 MEMORY_LIMIT=${MEMORY_LIMIT:-128M}
 UPLOAD_MAX=${UPLOAD_MAX:-8M}
 
+# Verificar se existe algum projeto no diretório /var/www/html
+
+# Substituir os placeholders nos arquivos de configuração
+
+# Instalar as extensões PHP necessárias
+
+# Criar o diretório para o arquivo sock
+
+# Definir as permissões corretas para o diretório do projeto
+
+
+
+# Check if the /var/www/html directory exists and is not empty
+if [ ! -d "$PROJECT_PATH" ] || [ -z "$(ls -A $PROJECT_PATH)" ]; then
+  echo "The directory '$PROJECT_PATH' is empty."
+  exit 1
+fi
+
+# Replace the PROJECT_PATH placeholder with the actual path
+sed -i "s|PROJECT_PATH|$PROJECT_PATH|" /etc/nginx/http.d/default.conf
+# Replace the CLIENT_MAX_BODY_SIZE placeholder with the actual value
+sed -i "s|UPLOAD_MAX|$UPLOAD_MAX|" /etc/nginx/http.d/default.conf
+
+# Replace the MEMORY_LIMIT placeholder with the actual value
+sed -i "s|MEMORY_LIMIT|$MEMORY_LIMIT|" /etc/php83/conf.d/settings.ini
+sed -i "s|MEMORY_LIMIT|$MEMORY_LIMIT|" /etc/php83/php-fpm.d/www.conf
+
+# Replace the POST_MAX_FILESIZE and POST_MAX_SIZE placeholders with the actual value
+sed -i "s|UPLOAD_MAX|$UPLOAD_MAX|" /etc/php83/conf.d/settings.ini
+
 # Get the list of already installed extensions via "php -m" command
 INSTALLED_EXTENSIONS=$(php -m | tr -d ' ' | sed 's/\[PHPModules\]//g' | sed 's/\[ZendModules\]//g')
 
@@ -26,29 +56,17 @@ if [ -n "${PHP_EXTENSIONS}" ]; then
   done
 fi
 
-# Check if the /var/www/html directory is empty
-if [ -z "$(ls -A /var/www/html)" ]; then
-  echo "The directory 'var/www/html' is empty"
+# Create directory for the sock file
+if ! mkdir -p /var/run/php; then
+  echo "Failed to create directory '/var/run/php'."
   exit 1
 fi
 
-# Replace the PROJECT_PATH placeholder with the actual path
-sed -i "s|PROJECT_PATH|$PROJECT_PATH|" /etc/nginx/http.d/default.conf
-# Replace the CLIENT_MAX_BODY_SIZE placeholder with the actual value
-sed -i "s|UPLOAD_MAX|$UPLOAD_MAX|" /etc/nginx/http.d/default.conf
-
-# Replace the MEMORY_LIMIT placeholder with the actual value
-sed -i "s|MEMORY_LIMIT|$MEMORY_LIMIT|" /etc/php83/conf.d/settings.ini
-sed -i "s|MEMORY_LIMIT|$MEMORY_LIMIT|" /etc/php83/php-fpm.d/www.conf
-
-# Replace the POST_MAX_FILESIZE and POST_MAX_SIZE placeholders with the actual value
-sed -i "s|UPLOAD_MAX|$UPLOAD_MAX|" /etc/php83/conf.d/settings.ini
-
-# Create directory for the sock file
-mkdir -p /var/run/php
-
 # Set the correct permissions for the project directory
-chown -R nginx:www-data /var/www/html /var/run/php
+if ! chown -R nginx:www-data /var/www/html /var/run/php; then
+  echo "Failed to set permissions for '/var/www/html' and '/var/run/php'."
+  exit 1
+fi
 
 # Start PHP-FPM and Nginx
 php-fpm & nginx -g "daemon off;"
