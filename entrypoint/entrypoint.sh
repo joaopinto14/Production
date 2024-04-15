b967300cef6d9c58ce23c62e68d5ed6c92b2c700
João Pinto <psp.joao@hotmail.com>
2024-04-15 18:57:41 +0100

    Corrigir bug existente na função "check_and_install_extension"

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -19,24 +19,31 @@ fi
 
 # Function to check and install PHP extensions
 check_and_install_extension() {
-  installed_extensions=$(php -m)
+  installed_extensions=$(php -m | tr '[:upper:]' '[:lower:]')
+  extensions_to_install=""
 
   for extension in ${PHP_EXTENSIONS}; do
+    # Convert extension name to lowercase
+    extension=$(echo "${extension}" | tr '[:upper:]' '[:lower:]')
     # Check if there is any uninstalled extension
     if ! echo "${installed_extensions}" | grep -wq "${extension}"; then
-      if [ -z "${extensions_to_install}" ]; then
-        extensions_to_install="${extension}"
-      else
-        extensions_to_install="${extensions_to_install} ${extension}"
-      fi
+      extensions_to_install="${extensions_to_install} ${extension}"
     fi
   done
 
   # Install uninstalled PHP extensions
   if [ -n "${extensions_to_install}" ]; then
-    echo "Installing PHP extensions: ${extensions_to_install}..."
+    echo "Installing PHP extensions:${extensions_to_install}..."
     for extension in ${extensions_to_install}; do
-      apk add -q --no-cache php83-"${extension}" > /dev/null 2>&1 || { echo "Failed to install PHP extension: ${extension}."; exit 1; }
+      apk add -q --no-cache php83-"${extension}" > /dev/null 2>&1
+    done
+    # Check if extensions were installed successfully
+    installed_extensions=$(php -m | tr '[:upper:]' '[:lower:]')
+    for extension in ${extensions_to_install}; do
+      if ! echo "${installed_extensions}" | grep -wq "${extension}"; then
+        echo "Failed to install PHP extension: ${extension}."
+        exit 1
+      fi
     done
     rm -rf /var/cache/apk/*
     echo "PHP extensions installed successfully."
