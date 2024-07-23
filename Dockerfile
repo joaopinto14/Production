2eb362914cac1660e40e907a07ccd82cef6dbcec
João Pinto <support@joaopinto.pt>
2024-07-23 14:22:35 +0100

    Code cleaning and optimizations (#6)
    * Code cleaning and optimizations

--- Dockerfile
+++ Dockerfile
@@ -2,35 +2,32 @@ FROM alpine
 
 LABEL maintainer="João Pinto [suport@joaopinto.pt]"
 
-# Install PHP, Nginx and Supervisor
-RUN apk update && apk add --no-cache \
-    php83 \
-    php83-fpm \
-    nginx \
-    supervisor \
-    tzdata && \
-    rm -rf /var/cache/apk/*
+# Install necessary packages
+RUN apk update && apk add --no-cache php83 php83-fpm nginx supervisor tzdata && rm -rf /var/cache/apk/*
 
-# Copy PHP configuration file
-COPY php/settings.ini /etc/php83/conf.d/settings.ini
-COPY php/www.conf /etc/php83/php-fpm.d/www.conf
+# PHP-FPM Configuration
+COPY php/settings.ini /etc/php83/conf.d/
+RUN sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php83/php.ini
+RUN sed -i 's/user = nobody/user = www/' /etc/php83/php-fpm.d/www.conf
+RUN sed -i 's/group = nobody/group = www/' /etc/php83/php-fpm.d/www.conf
+RUN sed -i 's/listen = 127.0.0.1:9000/listen = \/var\/run\/php-fpm.sock/' /etc/php83/php-fpm.d/www.conf
+RUN sed -i 's/;listen.owner = nobody/listen.owner = www/' /etc/php83/php-fpm.d/www.conf
+RUN sed -i 's/;listen.group = www/listen.group = www/' /etc/php83/php-fpm.d/www.conf
+RUN sed -i 's/;listen.mode = 0660/listen.mode = 0660/' /etc/php83/php-fpm.d/www.conf
 
-# Change names of PHP and PHP-FPM binaries
-RUN mv /usr/bin/php83 /usr/bin/php && \
-    mv /usr/sbin/php-fpm83 /usr/sbin/php-fpm
-
-# Copy Nginx configuration file and entrypoint script
-COPY nginx/default.conf /etc/nginx/http.d/default.conf
+# Nginx Configuration
+COPY nginx/default.conf /etc/nginx/http.d/
 COPY nginx/nginx.conf /etc/nginx/nginx.conf
-COPY entrypoint/entrypoint.sh /usr/local/bin/entrypoint.sh
 
-# Make the startup script executable
+# Rename PHP and PHP-FPM binaries
+RUN mv /usr/bin/php83 /usr/bin/php && mv /usr/sbin/php-fpm83 /usr/sbin/php-fpm
+
+# Copy entrypoint script and make it executable
+COPY entrypoint/entrypoint.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/entrypoint.sh
 
-# Copy Supervisor configuration file
-COPY supervisor/supervisord.conf /etc/supervisor/supervisord.conf
-
-# Create directory for Supervisor configuration files
+# Copy Supervisor configuration file and create necessary directories
+COPY supervisor/supervisord.conf /etc/supervisor/
 RUN mkdir -p /etc/supervisor/conf /var/log/supervisor
 
 # Add new user
@@ -39,8 +36,5 @@ RUN adduser -D -g 'www' www
 # Expose web server port and set healthcheck
 EXPOSE 80
 HEALTHCHECK --interval=60s --timeout=3s --start-period=5s --retries=3 CMD wget -q -O /dev/null http://localhost
-
-# Set the working directory
 WORKDIR /var/www/html
-
 CMD ["entrypoint.sh"]
--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -1,92 +1,106 @@
-#!/bin/ash
+#!/bin/sh
 
-# Define default values for environment variables if they are not set
+# Variables
+TIMEZONE=${TIMEZONE:-}
 PHP_EXTENSIONS=${PHP_EXTENSIONS:-}
-TIMEZONE=${TIMEZONE:-UTC}
-INDEX_PATH=${INDEX_PATH:-/var/www/html}
 MEMORY_LIMIT=${MEMORY_LIMIT:-128M}
 UPLOAD_MAX_SIZE=${UPLOAD_MAX_SIZE:-8M}
+INDEX_PATH=${INDEX_PATH:-/var/www/html}
 SUPERVISOR_CONF=${SUPERVISOR_CONF:-}
 
-# Define file paths
 NGINX_CONF="/etc/nginx/nginx.conf"
 NGINX_VIRTUAL_HOST_CONF="/etc/nginx/http.d/default.conf"
-PHP_INI="/etc/php83/conf.d/settings.ini"
-PHP_FPM_CONF="/etc/php83/php-fpm.d/www.conf"
+PHP_SETTINGS="/etc/php83/conf.d/settings.ini"
+PHP_WWW="/etc/php83/php-fpm.d/www.conf"
 
-# Check if the file index.php exists in the INDEX_PATH directory
-if [ ! -f "${INDEX_PATH}/index.php" ]; then
-  echo "The path specified in INDEX_PATH does not contain the 'index.php' file."
-  exit 1
-fi
+# Functions
 
-# Function to check and install PHP extensions
-check_and_install_extension() {
-  installed_extensions=$(php -m | tr '[:upper:]' '[:lower:]')
-  extensions_to_install=""
-
-  for extension in ${PHP_EXTENSIONS}; do
-    # Convert extension name to lowercase
-    extension=$(echo "${extension}" | tr '[:upper:]' '[:lower:]')
-    # Check if there is any uninstalled extension
-    if ! echo "${installed_extensions}" | grep -wq "${extension}"; then
-      extensions_to_install="${extensions_to_install} ${extension}"
-    fi
-  done
-
-  # Install uninstalled PHP extensions
-  if [ -n "${extensions_to_install}" ]; then
-    echo "Installing PHP extensions:${extensions_to_install}..."
-    for extension in ${extensions_to_install}; do
-      apk add -q --no-cache php83-"${extension}" > /dev/null 2>&1
-    done
-    # Check if extensions were installed successfully
-    installed_extensions=$(php -m | tr '[:upper:]' '[:lower:]')
-    for extension in ${extensions_to_install}; do
-      if ! echo "${installed_extensions}" | grep -wq "${extension}"; then
-        echo "Failed to install PHP extension: ${extension}."
+# Function to verify if the 'index.php' file exists in the path specified in INDEX_PATH
+verify_index_file() {
+    if [ ! -f "${INDEX_PATH}/index.php" ]; then
+        echo "The path specified in INDEX_PATH does not contain the 'index.php' file."
         exit 1
-      fi
-    done
-    rm -rf /var/cache/apk/*
-    echo "PHP extensions installed successfully."
-  fi
+    fi
 }
 
-# Set the timezone
-if [ -n "${TIMEZONE}" ]; then
-  ln -snf "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime || { echo "Failed to set timezone to '${TIMEZONE}'."; exit 1; }
-  echo "${TIMEZONE}" > /etc/timezone || { echo "Failed to set timezone to '${TIMEZONE}'."; exit 1; }
+# Function to set the timezone
+set_timezone() {
+    echo "Setting timezone to $TIMEZONE"
+    # Check if the timezone is valid
+    if [ -f "/usr/share/zoneinfo/$TIMEZONE" ]; then
+        cp "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime || { echo "Failed to set timezone"; exit 1;}
+        echo "$TIMEZONE" > /etc/timezone || { echo "Failed to set timezone"; exit 1;}
+    else
+        echo "Timezone '$TIMEZONE' is not valid"
+        exit 1
+    fi
+}
+
+# Function to install additional PHP extensions
+install_php_extensions() {
+    echo "Installing PHP extensions..."
+    installed_extensions=$(php -m | tr '[:upper:]' '[:lower:]')
+    php_extensions=$(echo "${PHP_EXTENSIONS}" | tr '[:upper:]' '[:lower:]')
+
+    # Install PHP extensions
+    for extension in ${php_extensions}; do
+        if ! echo "${installed_extensions}" | grep -wq "${extension}"; then
+            apk add -q --no-cache php83-"${extension}" > /dev/null 2>&1
+            if ! php -m | tr '[:upper:]' '[:lower:]' | grep -wq "${extension}"; then
+                echo "Failed to install PHP extension: ${extension}."
+                exit 1
+            fi
+        fi
+    done
+
+    # Clean cache
+    rm -rf /var/cache/apk/*
+    echo "PHP extensions installed successfully."
+}
+
+# Function to replace placeholders in configuration files
+replace_placeholders() {
+    sed -i "s|UPLOAD_MAX_SIZE|${UPLOAD_MAX_SIZE}|" ${NGINX_CONF} || { echo "Failed to replace placeholders in ${NGINX_CONF}."; exit 1; }
+    sed -i "s|INDEX_PATH|${INDEX_PATH}|;s|UPLOAD_MAX_SIZE|${UPLOAD_MAX_SIZE}|" ${NGINX_VIRTUAL_HOST_CONF} || { echo "Failed to replace placeholders in ${NGINX_VIRTUAL_HOST_CONF}."; exit 1; }
+    sed -i "s|MEMORY_LIMIT|${MEMORY_LIMIT}|;s|UPLOAD_MAX_SIZE|${UPLOAD_MAX_SIZE}|" ${PHP_SETTINGS} || { echo "Failed to replace placeholders in ${PHP_SETTINGS}."; exit 1; }
+    sed -i "s|MEMORY_LIMIT|${MEMORY_LIMIT}|" ${PHP_WWW} || { echo "Failed to replace placeholders in ${PHP_WWW}."; exit 1; }
+}
+
+# Function to set the Supervisor configuration
+set_config_supervisor() {
+    echo "Setting up Supervisor configuration..."
+    if [ -f "${SUPERVISOR_CONF}" ]; then
+        cp -f "${SUPERVISOR_CONF}" /etc/supervisor/conf/"${SUPERVISOR_CONF##*/}" || { echo "Failed to copy the file '${SUPERVISOR_CONF}' to '/etc/supervisor/conf.d/'."; exit 1; }
+    else
+        echo "Supervisor configuration not found at ${SUPERVISOR_CONF}."
+        exit 1
+    fi
+}
+
+# Function to set permissions for the project directory
+set_permissions_directory_project() {
+    chown -R www:www /var/www/html || { echo "Failed to set permissions for '/var/www/html'."; exit 1; }
+}
+
+
+# Main
+
+verify_index_file
+replace_placeholders
+
+if [ -n "$TIMEZONE" ]; then
+    set_timezone
 fi
 
-# Replace placeholders with actual values in configuration files
-sed -i "s|UPLOAD_MAX_SIZE|${UPLOAD_MAX_SIZE}|" ${NGINX_CONF} || { echo "Failed to replace placeholders in ${NGINX_CONF}."; exit 1; }
-sed -i "s|INDEX_PATH|${INDEX_PATH}|;s|UPLOAD_MAX_SIZE|${UPLOAD_MAX_SIZE}|" ${NGINX_VIRTUAL_HOST_CONF} || { echo "Failed to replace placeholders in ${NGINX_VIRTUAL_HOST_CONF}."; exit 1; }
-sed -i "s|MEMORY_LIMIT|${MEMORY_LIMIT}|;s|UPLOAD_MAX_SIZE|${UPLOAD_MAX_SIZE}|" ${PHP_INI} || { echo "Failed to replace placeholders in ${PHP_INI}."; exit 1; }
-sed -i "s|MEMORY_LIMIT|${MEMORY_LIMIT}|" ${PHP_FPM_CONF} || { echo "Failed to replace placeholders in ${PHP_FPM_CONF}."; exit 1; }
-
-# Check if the SUPERVISOR_CONF environment variable is set
-if [ -n "${SUPERVISOR_CONF}" ]; then
-  # Check if the file exists
-  if [ -f "${SUPERVISOR_CONF}" ]; then
-    cp -f "${SUPERVISOR_CONF}" /etc/supervisor/conf/"${SUPERVISOR_CONF##*/}" || { echo "Failed to copy the file '${SUPERVISOR_CONF}' to '/etc/supervisor/conf.d/'."; exit 1; }
-  else
-    echo "The file specified in SUPERVISOR_CONF does not exist."
-    exit 1
-  fi
+if [ -n "$PHP_EXTENSIONS" ]; then
+    install_php_extensions
 fi
 
-
-# Install the required PHP extensions
-if [ -n "${PHP_EXTENSIONS}" ]; then
-  check_and_install_extension
+if [ -n "$SUPERVISOR_CONF" ]; then
+    set_config_supervisor
 fi
 
-# Create directory for the sock file
-mkdir -p /var/run/php || { echo "Failed to create directory '/var/run/php'."; exit 1; }
-
-# Set the correct permissions for the project directory and the sock file directory
-chown -R www:www /var/www/html /var/run/php || { echo "Failed to set permissions for '/var/www/html' and '/var/run/php'."; exit 1; }
+set_permissions_directory_project
 
 # Start supervisord
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
